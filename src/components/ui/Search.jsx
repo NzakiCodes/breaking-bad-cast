ddf2ed3e77d3b30a9049d715313b3cec5c94ea1d
NzakiCodes <ekisnzaki@gmail.com>
2020-07-14 22:17:24 +0100

    Create Search.jsx

---
+++ src/components/ui/Search.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+    return (
+        <div className="search">
+            <form action="">
+                <input
+                    type="text"
+                    className="search"
+                    autoFocus
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Search
