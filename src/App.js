9198219708d874159b467edb2c90a2a4ecf7b40a
NzakiCodes <ekisnzaki@gmail.com>
2020-07-14 22:17:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import BB from './images/favicon.png'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
+
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+
+      console.log(result.data);
+
+      setCharacters(result.data);
+      setIsLoading(false);
+    }
+    fetchItems();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <img src={BB} alt="" />
-      <h3>Breaking Bad Cast-App</h3>
+      <Header />
+      <Search />
+      <CharacterGrid isLoading={isLoading} characters={characters} />
     </div>
   );
 }
