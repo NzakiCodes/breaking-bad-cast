c547d24dc3125d962b39269c685516d06464fcbb
NzakiCodes <ekisnzaki@gmail.com>
2020-07-14 23:07:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ import Search from './components/ui/Search'
 const App = () => {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+const [query, setQuery] = useState('')
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
 
-      console.log(result.data);
+      // console.log(result.data);
 
       setCharacters(result.data);
       setIsLoading(false);
     }
     fetchItems();
-  }, []);
+  }, [query]);
 
 
 
   return (
     <div className="App">
       <Header />
-      <Search />
+      <Search getQuery={(q)=> setQuery(q)} />
       <CharacterGrid isLoading={isLoading} characters={characters} />
     </div>
   );
